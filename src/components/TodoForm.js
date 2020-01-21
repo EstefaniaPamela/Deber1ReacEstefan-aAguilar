a9f2e1c9e37baecb0e9a3b79ef8c3d1ea86d3249
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2020-01-21 14:18:13 -0500

    Deber 1 reac

---
+++ src/components/TodoForm.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+    constructor () {
+        super();
+        this.state = {
+            title: '',
+            description: '',
+
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onAddTodo(this.state);
+        this.setState({
+            title: '',
+            description: '',
+
+        });
+    }
+
+    handleInputChange(e) {
+        const {value, name} = e.target;
+        console.log(value, name);
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <center>
+            <div className="card">
+                <form onSubmit={this.handleSubmit} className="card-body">
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="title"
+                            className="form-control"
+                            value={this.state.title}
+                            onChange={this.handleInputChange}
+                            placeholder="Titulo de la tarea"
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.handleInputChange}
+                            placeholder="Descripcion de la Tarea"
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">
+                        Crear Tarea
+                    </button>
+                </form>
+            </div>
+            </center>
+        )
+    }
+
+}
+
+export default TodoForm;
